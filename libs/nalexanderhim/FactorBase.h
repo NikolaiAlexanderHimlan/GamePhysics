56770fbbcf886c397d2b1a88fb26cf3cd7528c25
Nikolai Alexander-Himlan <Nikolai.AlexanderHimlan@gmail.com>
2015-03-24 00:46:31 -0400

    fixed error caused by dividing by 0 in FactorBase
    tried to implement less frequent Factor calculation but was unable, left implementation disabled and note

--- libs/nalexanderhim/FactorBase.h
+++ libs/nalexanderhim/FactorBase.h
@@ -19,41 +19,62 @@ I certify that this assignment is entirely my own work.
 
 //TODO: CONSIDER: maybe this should be a Template T class instead, but that doesn't forcefully limit it to numbers
 //TODO: CONSIDER: is this factor or inverse?
+//TODO: the following TODO does not currently work, as RefreshFactor cannot be called in getFactor due to const, implementation left but disabled.  //TODO: use boolean to track if mValue has been changed and calculate mFactor only in getFactor and only if mValue has changed
 
 class FACTOR_CLASS
 {
 private:
+	bool mChanged = false;
 	VALUE_TYPE mValue;
 	FACTOR_TYPE mFactor;
+
+	inline void RefreshFactor()
+	{
+		if (mChanged)
+		{
+			if (mValue == VALUE_ZERO)
+				mFactor = FACTOR_ZERO;
+			else
+				mFactor = FACTOR_ONE / mValue;
+
+			mChanged = false;
+		}
+	}
 public:
 	FACTOR_CLASS(VALUE_TYPE val = VALUE_ZERO) { setValue(val); };
 	FACTOR_CLASS(const FACTOR_CLASS& rhs) { setValue(rhs.getValue()); };
 
 	//Getters
-	inline VALUE_TYPE getValue() const { return mValue; };
-	inline FACTOR_TYPE getFactor() const { return mFactor; };
+	inline const VALUE_TYPE& getValue() const { return mValue; };
+	inline FACTOR_TYPE getFactor() const {
+		//RefreshFactor();
+		return mFactor;
+	};
 
 	//Setters
 	inline void setValue(VALUE_TYPE newValue)
 	{
 		mValue = newValue;
-		mFactor = FACTOR_ONE / mValue;
+		mChanged = true;
+		RefreshFactor();//NOTE: unable to call in getFactor as intended due to const
 	}
 	inline void setFactor(FACTOR_TYPE newFactor)
 	{
 		mFactor = newFactor;
+		mValue = VALUE_ZERO;
+		if(newFactor != FACTOR_ZERO)
 		mValue = FACTOR_ONE / mFactor;
 	}
 
 	//Accessors
-// 	inline VALUE_TYPE& Value() { return &mValue;	};
+// 	inline VALUE_TYPE& Value() { mChanged = true; return &mValue;	};
 // 	inline FACTOR_TYPE& Factor() { return &mFactor	};
 
 	//Operators
-	inline FACTOR_CLASS& operator =(const VALUE_TYPE& rhs) { setValue(rhs); return *this; };
-	inline FACTOR_CLASS& operator =(const FACTOR_CLASS& rhs) { setValue(rhs.getValue()); return *this; };
+	inline const FACTOR_CLASS& operator =(const VALUE_TYPE& rhs) { setValue(rhs); return *this; };
+	inline const FACTOR_CLASS& operator =(const FACTOR_CLASS& rhs) { setValue(rhs.getValue()); return *this; };
 
 	//Conversion Operators
-	operator VALUE_TYPE() const { return getValue(); };
+	operator const VALUE_TYPE&() const { return getValue(); };
 };
 #endif
--- libs/nalexanderhim/IntegerFactor.h
+++ libs/nalexanderhim/IntegerFactor.h
@@ -7,10 +7,18 @@ Assignment: pa 2
 Certification of Authenticity:
 I certify that this assignment is entirely my own work.
 */
+#undef FACTOR_CLASS
+#undef VALUE_TYPE
+#undef VALUE_ZERO
+#undef FACTOR_TYPE
+#undef FACTOR_ZERO
+#undef FACTOR_ONE
+
 #define FACTOR_CLASS floatFactor /**/
 #define VALUE_TYPE int /**/
 #define VALUE_ZERO 0 /*The default constructor value*/
 #define FACTOR_TYPE float /**/
+#define FACTOR_ZERO (0.0f /*Default value*/)
 #define FACTOR_ONE 1.0f /*What to divide by value to get the factor*/
 
 #include "FactorBase.h"
