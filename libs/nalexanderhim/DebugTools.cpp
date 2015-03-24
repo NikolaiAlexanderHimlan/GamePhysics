e99b26e70918bd5fcee914a5cd3dfba2e045ea83
Nikolai Alexander-Himlan <Nikolai.AlexanderHimlan@gmail.com>
2015-03-23 21:14:58 -0400

    several failed attempts at getting glut text working

--- libs/nalexanderhim/DebugTools.cpp
+++ libs/nalexanderhim/DebugTools.cpp
@@ -38,7 +38,52 @@ void Debug_OpenGL::outputText(const Vector3f& location, const nah::Color& color,
 	const char* charText = text;// text.c_str();
 	int len = (int)strlen(charText);
 
+	/*added 1
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	glOrtho(0, getWindowWidth, 0, getWindowHeight, -1.0f, 1.0f);
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glLoadIdentity();
+	glPushAttrib(GL_DEPTH_TEST);
+	glDisable(GL_DEPTH_TEST);
+	
+	//plus 3
+	glDisable(GL_TEXTURE_2D);
+	//*/
+	/*added 2
+
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	gluOrtho2D(0.0, getWindowWidth(), 0.0, getWindowHeight());
+
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glLoadIdentity();
+	//*/
+	//added 4
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
 	glColor3f(color.rgbRed(), color.rgbGreen(), color.rgbBlue());
+
+
+	/*added 4
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	gluOrtho2D(0, getWindowWidth(), 0, getWindowHeight());
+	glScalef(1, -1, 1);
+	glTranslatef(0, -getWindowHeight(), 0);
+	glMatrixMode(GL_MODELVIEW);
+
+
+	glPushMatrix();
+	glLoadIdentity();
+	//*/
+
 
 	//*2D Text
 	//(x,y) is from the bottom left of the window
@@ -52,4 +97,30 @@ void Debug_OpenGL::outputText(const Vector3f& location, const nah::Color& color,
 		glutBitmapCharacter(font, charText[i]);
 		//glutStrokeCharacter(font, charText[i]);
 	}
+
+	/*added 1
+	glPopAttrib();
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
+	//*/
+	/*added 2
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
+
+
+	//plus 3
+	glEnable(GL_TEXTURE_2D);
+	//*/
+	/*added 4
+	glPopMatrix();
+
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+	//*/
 }
