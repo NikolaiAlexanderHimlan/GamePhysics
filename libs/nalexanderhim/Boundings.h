225e60fac426748fc0683b90eddde008e43dd538
Nikolai Alexander-Himlan <Nikolai.AlexanderHimlan@gmail.com>
2015-03-24 20:55:45 -0400

    created Sphere Volume
    created SphereBounding and started on PlaneBounding

--- libs/nalexanderhim/Boundings.h
+++ libs/nalexanderhim/Boundings.h
@@ -47,4 +47,75 @@ struct Bounding
 	};
 };
 
+//HACK: walls are currently essentially infinite
+//HACK: impassable is currently not implemented
+struct PlaneBounding
+	: public Bounding
+{
+	static const BoundType BOUND_TYPE = PLANE;
+
+	float width, length;
+	Vector3f normal;
+	bool impassable = false;//is it possible for an object to be on the opposite side of the wall from the normal?
+
+	virtual inline bool CheckOverlap(const Bounding& otherBounds, const Vector3f& boundsLocation, const Vector3f& otherLocation) const
+	{
+		//Generate a vector with the correct location along the normal and otherLocation filling in the remaining data
+		//NOTE: the closer to 1 the axis value in normal is, the less relevant otherLocation is
+		//TODO: limit the range of wallPoint based on the width and length
+		Vector3f wallPoint = 
+			Vector3f::Multiply(boundsLocation, normal)//location of bounds is is directly relevant based on the normal
+			+ Vector3f::Multiply(otherLocation, normal.InvertedNormal());//other location is inversely relevant based on the normal
+
+		//checkLocation on opposite side from normal
+		//TODO: handle impassable
+		if (impassable)
+		{
+			LOGIC_ERR("impassible not implemented - PlanesBounding - Contains");
+		}
+
+		return otherBounds.Contains(otherLocation, wallPoint);
+	};
+
+	virtual inline bool Contains(const Vector3f& boundsLocation, const Vector3f& checkLocation) const
+	{
+		bool doesContain = false;
+
+		//checkLocation on opposite side from normal
+		//TODO: handle impassable
+		if (impassable)
+		{
+			LOGIC_ERR("impassible not implemented - PlanesBounding - Contains");
+		}
+		return doesContain;
+	};
+};
+
+#include "VolumeDefinition.h"
+struct SphereBounding
+	: public Bounding, public SphereVolume
+{
+	static const BoundType BOUND_TYPE = SPHERE;
+
+	virtual inline bool CheckOverlap(const Bounding& otherBounds, const Vector3f& boundsLocation, const Vector3f& otherLocation) const
+	{
+		//1st, calculate a vector to otherLocation with radius as the length.
+		Vector3f vectTo = Vector3f::Distancepoint(boundsLocation, otherLocation, radius);
+		
+		//2nd, check if this vector passes the otherLocation, in which case there is definitely a collision
+		if(Vector3f::Between(boundsLocation, vectTo, otherLocation))
+			return true;
+
+		//3rd, check if the other bounds contains the location indicated by the calculated vector
+		//return otherBounds.Contains(otherLocation, vectTo);//cannot call directly due to inheritance funnyness, calling Bounding CheckOverlap
+		return otherBounds.Contains(otherLocation, vectTo);
+	}
+
+	virtual inline bool Contains(const Vector3f& boundsLocation, const Vector3f& checkLocation) const
+	{
+		//check if distance to checkLocation is less than radius, return this as the result
+		return std::powf(radius, 2.0f) > Vector3f::DistanceSquared(boundsLocation, checkLocation);
+	}
+};
+
 #endif // Boundings_h__
--- libs/nalexanderhim/VolumeDefinition.h
+++ libs/nalexanderhim/VolumeDefinition.h
@@ -17,4 +17,32 @@ struct VolumeDefinition
 	{ return HighestPoint(volumePos, upAxis) * -1.0f; };
 };
 
+struct SphereVolume
+	: public VolumeDefinition
+{
+	float radius = 0.0f;
+	inline Vector3f HighestPoint(const Vector3f& volumePos = Vector3f::zero, Axis upAxis = UP) const
+	{
+		Vector3f highPoint;
+
+		switch (upAxis)
+		{
+		case X:
+			highPoint = Vector3f(radius, 0.0f, 0.0f);
+			break;
+		case Y:
+			highPoint = Vector3f(0.0f, radius, 0.0f);
+			break;
+		case Z:
+			highPoint = Vector3f(0.0f, 0.0f, radius);
+			break;
+		default:
+			throw std::logic_error("Invalid Axis - SphereVolume - LowestPoint!");
+			break;
+		}
+
+		return volumePos + highPoint;
+	};
+};
+
 #endif // VolumeDefinition_h__
