8aeaee0eb28cf8400a7e3c57e30c6b45b7b8dfa3
Nikolai Alexander-Himlan <Nikolai.AlexanderHimlan@gmail.com>
2015-03-26 02:23:40 -0400

    updated certification header cyclone attribution

--- libs/nalexanderhim/ParticleContactGenerator.h
+++ libs/nalexanderhim/ParticleContactGenerator.h
@@ -6,7 +6,7 @@ Class: EPG-425 <Section 51>
 Assignment: pa 3
 Certification of Authenticity:
 I certify that this assignment is entirely my own work.
-Based on Ian Millington's cyclone
+Based on Ian Millington's cyclone physics engine.
 */
 class ParticleContact;
 
--- libs/nalexanderhim/ParticleRod.cpp
+++ libs/nalexanderhim/ParticleRod.cpp
@@ -4,7 +4,7 @@ Class: EPG-425 <Section 51>
 Assignment: pa 3
 Certification of Authenticity:
 I certify that this assignment is entirely my own work.
-Based on Ian Millington's cyclone
+Based on Ian Millington's cyclone physics engine.
 */
 #include "ParticleRod.h"
 #include "ParticleContact.h"
