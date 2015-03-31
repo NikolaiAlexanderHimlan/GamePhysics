8051c5f138f7d8e84d7457ef2df9f14e56fe75cc
Nikolai Alexander-Himlan <Nikolai.AlexanderHimlan@gmail.com>
2015-03-31 08:55:07 -0400

    started work on ParticleSprings (and Bungees)

---
+++ libs/nalexanderhim/ParticleBungee.h
@@ -0,0 +1,11 @@
+#ifndef ParticleBungee_h__
+#define ParticleBungee_h__
+/*
+Author: Nikolai Alexander-Himlan
+Class: EPG-425 <Section 51>
+Assignment: pa 3
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+Based on Ian Millington's cyclone physics engine.
+*/
+#endif // ParticleBungee_h__
--- libs/nalexanderhim/ParticleSpring.h
+++ libs/nalexanderhim/ParticleSpring.h
@@ -8,11 +8,27 @@ Certification of Authenticity:
 I certify that this assignment is entirely my own work.
 Based on Ian Millington's cyclone physics engine.
 */
-#include "ParticleLink.h"
+#include "ParticleForceGenerator.h"
+#include "MathDefines.h"
 
 class ParticleSpring
-	: public ParticleLink
+	: public ParticleForceGenerator
 {
+	/** The particle at the other end of the spring. */
+	Particle* mpSpringEnd;
+
+	/** Holds the spring constant. */
+	real mSpringConstant;
+
+	/** Holds the rest length of the spring. */
+	real mRestingLength;
+
+public:
+	/** Creates a new spring with the given parameters. */
+	ParticleSpring(Particle* other, real springConstant, real restLength);
+
+	/** Applies the spring force to the given particle. */
+	virtual void updateForce(Particle *particle, real duration);
 };
 
 #endif // ParticleSpring_h__
